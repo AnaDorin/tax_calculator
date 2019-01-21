650a4c3adc0dea0d2b9017c58294c890a3bc337c
georgia carr <georgia.carr@autotrader.co.uk>
2019-01-21 14:13:25 +0000

    alternate fuel tests now work woo!

--- exercises/java/src/test/java/tax/ActualTaxCalculator.java
+++ exercises/java/src/test/java/tax/ActualTaxCalculator.java
@@ -2,7 +2,7 @@ package tax;
 
 public class ActualTaxCalculator extends TaxCalculator {
 
-    public int calculateTax(Vehicle vehicle) {
+    public int calculateTax ( Vehicle vehicle ) {
         if (vehicle.getCo2Emissions() == 0 & vehicle.getFuelType().equals(FuelType.PETROL)) {
             return 0;
         }
@@ -41,6 +41,49 @@ public class ActualTaxCalculator extends TaxCalculator {
         }
         if (vehicle.getCo2Emissions() > 255 & vehicle.getFuelType().equals(FuelType.PETROL)) {
             return 2070;
+        }
+        if (vehicle.getCo2Emissions() <= 0 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 0;
+        }
+        if (vehicle.getCo2Emissions() <= 50 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 0;
+        }
+        if (vehicle.getCo2Emissions() <= 75 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 15;
+        }
+        if (vehicle.getCo2Emissions() <= 90 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 95;
+        }
+        if (vehicle.getCo2Emissions() <= 100 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 115;
+        }
+        if (vehicle.getCo2Emissions() <= 110 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 135;
+        }
+        if (vehicle.getCo2Emissions() <= 130 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 155;
+        }
+        if (vehicle.getCo2Emissions() <= 150 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 195;
+        }
+        if (vehicle.getCo2Emissions() <= 170 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 505;
+        }
+        if (vehicle.getCo2Emissions() <= 190 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 820;
+        }
+        if (vehicle.getCo2Emissions() <= 225 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 1230;
+        }
+        if (vehicle.getCo2Emissions() <= 255 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 1750;
+        }
+        if (vehicle.getCo2Emissions() > 255 & vehicle.getFuelType().equals(FuelType.ALTERNATIVE_FUEL)) {
+            return 2060;
+        }
+
+        if (vehicle.getFuelType().equals(FuelType.ELECTRIC)) {
+            return 0;
         }
         return -1;
     }
